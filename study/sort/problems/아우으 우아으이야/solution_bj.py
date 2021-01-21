b354f5cca899ddeb664d974d4c0de36e82901fed
fxnnxc <beomjin26@naver.com>
2021-01-21 23:47:16 +0900

    .

---
+++ study/sort/problems/아우으 우아으이야/solution_bj.py
@@ -0,0 +1,24 @@
+import sys 
+N = int(input())
+lst = []
+for i in range(N):
+    x,y = map(int, sys.stdin.readline().strip().split())
+    lst.append([x,y])
+lst.sort()
+
+length = 0
+start = lst[0][0]
+last = lst[0][1]
+
+for i in range(1, len(lst)):
+    x,y = lst[i]
+    # overlap 
+    if x<=last:
+        last = max(y, last)
+    # skip
+    else:
+        length += last - start 
+        start, last = x, y
+
+length += last - start 
+print(length)
